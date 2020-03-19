8df7cc46cbb585b897316d2abe9c1dd04f471eb2
Duneya Saleh <29269@ma-web.nl>
2020-03-19 14:20:31 +0100

    Fotogallery

---
+++ java.js
@@ -0,0 +1,45 @@
+const kpopFoto =[
+    'https://chingutotheworld.com/wp-content/uploads/2019/10/E-L-F-JAPAN-FESTIVAL-2019-SJ-SCHOOL-super-junior-42873011-1165-773.jpg',
+    'https://cynthia0209.files.wordpress.com/2015/03/cnblue-ear-fun4.jpg',
+    'https://www.helloasia.com.au/wp-content/uploads/2018/10/SHINee-e1515477243173.jpg',
+    'https://i1.wp.com/ulzza.com/wp-content/uploads/2019/07/iKONs-First-Public-Appearance-As-A-6-Member-Group.jpg?fit=700%2C393&ssl=1',
+    'https://hypebeast.com/wp-content/blogs.dir/6/files/2018/09/the-top-10-k-pop-groups-bts-blackpink-exo-ikon-111.jpg',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-2.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-3.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-4.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-6.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-9.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://image-cdn.hypb.st/https%3A%2F%2Fbae.hypebeast.com%2Ffiles%2F2018%2F09%2Fthe-top-10-k-pop-groups-bts-blackpink-exo-ikon-8.jpg?q=90&w=1090&cbr=1&fit=max',
+    'https://pbs.twimg.com/media/Ct4rNdtWAAA0nIY.jpg:large',
+    'https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/styles/full/public/kard-18.jpg?itok=dCbgzUwd&mtime=1500265663',
+    'https://pm1.narvii.com/6494/e9ec50a92acc9b1e6a3941b8951999f273a2ba0e_hq.jpg',
+    'https://66.media.tumblr.com/ded163e478dd41e9c960e9e0f47a576b/tumblr_nei5ootNrb1tsqt43o2_640.jpg',
+    'https://www.allkpop.com/upload/2018/01/af_org/29141018/misc.jpg',
+    'https://pbs.twimg.com/media/DC7k-CKWsAA2NTb.jpg',
+    'https://lh3.googleusercontent.com/2OlgMwS9fxLnz4aivMEW6MKdvc7TWcnNnrWMANyEj1ifStXhKUJMkosOtvbOcXaEL-cX_m8ONJABKvkE3kKwVGLyEoewQMXetQ=w1200-rw',
+     'https://thumb.iyaa.com/780x500/smart/https://www.kholic.id/article/2018/02/__icsFiles/thumbnail/2018/02/02/0001-04-front.jpg',
+     'https://whatthekpop.com/wp-content/uploads/2016/12/ss501-podr-a-hacer-su-comeback-para-el-a-os-2016-732x377.jpg',
+     'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4513ae4b-4d78-4939-b7af-71b8561d6e29/dbsbmpa-58568018-1d3a-4907-a4d7-57dffa99ea02.png/v1/fill/w_1000,h_500,strp/super_junior_play_png__1__103117__by_roxyneoncolors_dbsbmpa-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTAwIiwicGF0aCI6IlwvZlwvNDUxM2FlNGItNGQ3OC00OTM5LWI3YWYtNzFiODU2MWQ2ZTI5XC9kYnNibXBhLTU4NTY4MDE4LTFkM2EtNDkwNy1hNGQ3LTU3ZGZmYTk5ZWEwMi5wbmciLCJ3aWR0aCI6Ijw9MTAwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.jn1t-o6tS6cjavsXp-gA9TkVvzSivH0rAJuS7GxUL4E',
+      'https://0.soompi.io/wp-content/uploads/2017/11/09204928/ASTRO-1.jpg?s=900x600&e=t',
+      'https://pbs.twimg.com/media/DNR5AXVV4AAQ2qB.jpg',
+      'https://nos.nl/data/image/2019/11/04/590105/1920x1080a.jpg',
+      'https://avatanplus.com/files/resources/original/596e50682fd6615d56ea1704.png'
+    ];
+
+const kader = document.getElementById('container');
+
+function fotoeen(url) {
+    let item = document.createElement('div');
+    item.className = 'item';
+    let mijnFoto = document.createElement('img');
+    mijnFoto.src = url;
+    mijnFoto.alt = 'ventie';
+    item.append(mijnFoto);
+    kader.append(item);
+}
+///met i-- kan ik beginnen van beneden naar boven 
+for(let i=kpopFoto.length;  i>0; i--) {
+    let num = Math.floor (Math.random()*kpopFoto.length)
+fotoeen(kpopFoto[num] );
+kpopFoto.splice(num,1)
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Foto galerij
+### 19-03-2020
+http://29269.hosts2.ma-cloud.nl/ma/bewijzenmap/periode3/FRO/fotogallery/
